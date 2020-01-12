3dc9a9ba6d9d24e523d17c9a0664e634d46c9366
sbal13 <sbal13@gmail.com>
2020-01-11 22:52:24 -0500

    making models

---
+++ sequelize.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize')
+const UserModel = require('./models/user')
+// const WatchedMovieModel = require('./models/watched_movie')
+const MovieModel = require('./models/movie')
+
+const sequelize = new Sequelize('codementor', 'root', 'root', {
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+const User = UserModel(sequelize, Sequelize)
+const WatchedMovie = sequelize.define('watched_movie', {})
+const Movie = MovieModel(sequelize, Sequelize)
+
+User.belongsToMany(Movie, { through: WatchedMovie, unique: false })
+Movie.belongsToMany(User, { through: WatchedMovie, unique: false })
+
+
+sequelize.sync({ force: true })
+  .then(() => {
+    console.log(`Database & tables created!`)
+  })
+
+module.exports = {
+  User,
+  Movie
+}
