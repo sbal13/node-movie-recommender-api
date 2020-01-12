c214e670cf7598fb587c416eb65d81c34bd34bf5
Sai Kandala <sai.kandala@amcnetworks.com>
2020-01-12 00:57:19 -0500

    sends cors header response

--- live/index.js
+++ live/index.js
@@ -3,9 +3,11 @@ const app = require('express')();
 const bodyParser = require('body-parser')
 const router = require('./api/router');
 const port = 3000;
+const cors = require('cors');
 
 /* initialize api routes */
 app.use(bodyParser.json())
+app.use(cors());
 app.use(router.init());
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
